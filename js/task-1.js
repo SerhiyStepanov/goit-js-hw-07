3889e9b26f6d67471e9dfaa873681a337be5a7d2
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-09-15 21:03:46 +0300

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,18 @@
-const a = {
-  a: 10,
-  b: 20,
-};
+const navElItems = document.querySelectorAll('.item');
+// console.log(navElItems.length);
 
-confirm.log(a);
+const navElItemTitle = document.querySelector('h2');
+// console.log(`Категория: ${navElItemTitle.textContent}`);
+
+const navElCount = document.querySelector('.item');
+// console.log(`Количество элементов: ${navElCounnt.lastElementChild.children.length}`,
+
+const getElItem = ul => `В списке ${navElItems.length} категории.`;
+console.log(getElItem());
+
+const getElTitle = () => `Категория: ${navElItemTitle.textContent}`;
+console.log(getElTitle());
+
+const getElItemCount = () =>
+  `Количество элементов: ${navElCount.lastElementChild.children.length}`;
+console.log(getElItemCount());
