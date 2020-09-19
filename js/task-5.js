71f5130dfdb82fd6bea087f4b0dc3190d20316bf
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-09-19 23:55:07 +0300

    task-5 50%

--- js/task-5.js
+++ js/task-5.js
@@ -1,17 +1,19 @@
+// const inputEl = document.querySelector('#name-input');
+// console.log(inputEl);
+// const outputEl = document.querySelector('#name-output');
+// console.log(outputEl);
+
 const refs = {
   inputEl: document.querySelector('#name-input'),
   outputEl: document.querySelector('#name-output'),
 };
 
-const inputEl = document.querySelector('#name-input');
-console.dir(refs.inputEl);
-const outputEl = document.querySelector('#name-output');
-console.dir(refs.outputEl);
-
 refs.inputEl.addEventListener('input', oninputChange);
 
 function oninputChange(event) {
-  // console.log(event.currentTarget.value);
+  console.log(event.currentTarget.value);
 
-  refs.outputEl.textContent = event.currentTarget.value;
+  if (event.currentTarget.value !== '') {
+    refs.outputEl.textContent = event.currentTarget.value;
+  }
 }
